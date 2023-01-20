ef416b135479560d9b5d91aa597eca0d6a447e53
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-20 16:25:14 +0100

    header added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+    data() {
+        return {}
+    },
+    components: {
+        AppHeader: AppHeader,
+    }
+}
 
 </script>
 
 <template>
-
+    <app-header></app-header>
 </template>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+body {
+    font-family: Open Sans;
+    color: #000;
+}
+
+.container {
+    max-width: 1048px;
+    margin: 0 auto;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,77 @@
 <script>
-
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            image: 'dc-logo.png',
+            tools: [
+                { text: 'CHARACTERS', url: '#' },
+                { text: 'COMICS', url: '#' },
+                { text: 'MOVIES', url: '#' },
+                { text: 'TV', url: '#' },
+                { text: 'GAMES', url: '#' },
+                { text: 'COLLECTIBLES', url: '#' },
+                { text: 'VIDEOS', url: '#' },
+                { text: 'FANS', url: '#' },
+                { text: 'NEWS', url: '#' },
+                { text: 'SHOP', url: '#' },
+            ]
+        }
+    },
+    methods: {
+        buildImg(image) {
+            const url = new URL(`../assets/img/${image}`, import.meta.url);
+            return url.href;
+        }
+    }
+}
 </script>
 
 <template>
-
+    <div class="container flex-header">
+        <div>
+            <figure>
+                <a href="#"><img :src="buildImg(image)" alt="dc-logo"></a>
+            </figure>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li v-for="voice in tools">
+                        <a href="{{ voice.url }}">{{ voice.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.flex-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30vh;
+}
 
+ul {
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none;
+    // height: auto;
+
+    li {
+        a {
+            text-decoration: none;
+            color: #000;
+            padding: 0.75rem;
+            font-size: 0.70rem;
+        }
+    }
+}
+
+li a:hover {
+    color: #0282F9;
+    border-bottom: solid 2px #0282F9;
+}
 </style>
